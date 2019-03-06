16b798bd8e3bec56f53592df8ba497dc697107b6
kouyuanyang <1697315180@qq.com>
2019-03-06 10:42:14 +0000

    优化页面1次

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ Route::get('/', function () {
 });
 
 Route::get('/', 'StaticPagesController@home')->name('home');
-Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
