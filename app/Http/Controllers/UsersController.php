ba2dbdaa5fb2f48096fd5013c90bb360254dbd4c
kouyuanyang <1697315180@qq.com>
2019-03-14 09:37:28 +0000

    SCSS错误1次

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,13 @@ use Auth;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',[
+            'except' =>['show','create','store']
+        ]);
+    }
+    
     public function create()
     {
         return view('users.create');
@@ -38,11 +45,13 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update'.$user);
         return view('users.edit',compact('user'));
     }
 
     public function update(User $user,Request $request)
     {
+        $this->authorize('update',$user);
         $this->validat($request,[
             'name'=>'required|max:50',
             'password'=>'nullable|confirmed|min:6'
@@ -55,7 +64,7 @@ class UsersController extends Controller
         }
         $user->update($data);
 
-        session()->flash('success','个人资料更新');
-        return redirect()->route('users.show',$user);
+        session()->flash('success','个人资料更新成功!');
+        return redirect()->route('users.show',$user->id);
     }
 }
